f11fcfbd3b554b993d7de12ae561a32357a2003a
Jacob Erdman <jacob.erdman2147@gmail.com>
2020-02-10 11:47:22 -0800

    Added equals and hashCode functions to LinkedRoom to prepare for isTraversable implementation within maze

--- src/triviaMaze/room/LinkedRoom.java
+++ src/triviaMaze/room/LinkedRoom.java
@@ -1,17 +1,21 @@
 package triviaMaze.room;
 
+import java.util.UUID;
+
 public class LinkedRoom extends IRoom
 {
-	IRoomLink right;
-	IRoomLink left;
-	IRoomLink up;
-	IRoomLink down;
+	private IRoomLink right;
+	private IRoomLink left;
+	private IRoomLink up;
+	private IRoomLink down;
+	private UUID uuid;
 	
 	public LinkedRoom() {
 		right = new RoomLink();
 		left = new RoomLink();
 		up = new RoomLink();
 		down = new RoomLink();
+		uuid = UUID.randomUUID();
 	}
 	
 	@Override
@@ -55,6 +59,21 @@ public class LinkedRoom extends IRoom
 		default:
 			throw new IllegalArgumentException("Invalid direction passed into a directional function of LinkedRoom");
 		}
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (!(obj instanceof LinkedRoom)) {
+			return false;
+		}
+		LinkedRoom castedObj = (LinkedRoom)obj;
+		return (this.uuid.equals(castedObj.uuid));
+	}
+	
+	@Override
+	public int hashCode() {
+		return uuid.hashCode();
 	}
 
 }
