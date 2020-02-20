5aee1653592bbb2d5b0457f0408bd7ea0e4f32b7
Jacob Erdman <jacob.erdman2147@gmail.com>
2020-02-20 12:35:33 -0800

    Added controls to the player to allow them to traverse the maze with the wasd keys. The input currently is based on readLine due to lack of any better option

--- src/triviaMaze/TriviaMazeMain.java
+++ src/triviaMaze/TriviaMazeMain.java
@@ -1,18 +1,33 @@
 package triviaMaze;
 
+import java.io.*;
 import triviaMaze.room.*;
 import triviaMaze.maze.*;
+import triviaMaze.keyService.*;
+import triviaMaze.player.*;
 
 public class TriviaMazeMain
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		IMaze testMaze = new LinkedRectangularMaze(5,5);
 		IRoom start = testMaze.getStart();
 		testMaze.getEnd().disable("up");
 		testMaze.getEnd().disable("left");
 		System.out.println(testMaze.isTraversable(start));
+		
+		IPlayer player = new BasicPlayer(testMaze.getStart(), testMaze.getEnd());
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line = "";
+
+		   while (line.equalsIgnoreCase("exit") == false) {
+		       line = in.readLine();
+		       if (line.length() == 1) TmKeyService.keyPressed(line);
+		   }
+
+		   in.close();
 	}
 
 }
--- src/triviaMaze/player/BasicPlayer.java
+++ src/triviaMaze/player/BasicPlayer.java
@@ -1,10 +1,67 @@
 package triviaMaze.player;
 
 import triviaMaze.room.IRoom;
+import triviaMaze.keyService.*;
 
 public class BasicPlayer implements IPlayer
 {
 	private IRoom cur;
+	private IRoom end;
+	
+	public BasicPlayer(IRoom start, IRoom end) {
+		this.end = end;
+		cur = start;
+		addKeyHandlers();
+	}
+	
+	private void addKeyHandlers() {
+		TmKeyService.addHandler(new KeyHandler('a'){
+			private String direction = "left";
+			@Override
+			public void fire() {
+				if (cur.isEnabled(direction)) {
+					cur = cur.getRoom(direction);
+					System.out.println("Moved " + direction);
+				}
+			}
+		});
+		TmKeyService.addHandler(new KeyHandler('d'){
+			private String direction = "right";
+			@Override
+			public void fire() {
+				if (cur.isEnabled(direction)) {
+					cur = cur.getRoom(direction);
+					System.out.println("Moved " + direction);
+				}
+			}
+		});
+		TmKeyService.addHandler(new KeyHandler('w'){
+			private String direction = "up";
+			@Override
+			public void fire() {
+				if (cur.isEnabled(direction)) {
+					cur = cur.getRoom(direction);
+					System.out.println("Moved " + direction);
+				}
+			}
+		});
+		TmKeyService.addHandler(new KeyHandler('s'){
+			private String direction = "down";
+			@Override
+			public void fire() {
+				if (cur.isEnabled(direction)) {
+					cur = cur.getRoom(direction);
+					System.out.println("Moved " + direction);
+				}
+			}
+		});
+		TmKeyService.addHandler(new KeyHandler(' '){
+			@Override
+			public void fire() {
+				System.out.println("At exit? " + (cur.equals(end)));
+			}
+		});
+	}
 
 	@Override
 	public IRoom getRoom()
