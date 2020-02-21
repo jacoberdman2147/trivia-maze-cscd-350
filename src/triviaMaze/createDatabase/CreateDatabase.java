4cfdc2f9e4f9ad7d7518516ca9e1cad64bfa323e
RandyHec <Randy.80@hotmail.com>
2020-02-21 12:55:27 -0800

    Update CreateDatabase.java
    Updated database so that it only creates the table if it doesn't already exists. It also only adds those tuples if they can be added i.e. the ID isn't already in the table.

--- src/triviaMaze/createDatabase/CreateDatabase.java
+++ src/triviaMaze/createDatabase/CreateDatabase.java
@@ -13,7 +13,7 @@ public class CreateDatabase {
 		System.out.println("Opened database successfully.");
 		
 		stmt = c.createStatement();
-		String sql = "CREATE TABLE MULTIPLE " +
+		String sql = "CREATE TABLE IF NOT EXISTS MULTIPLE " +
 					 "(ID	INT	PRIMARY	KEY	NOT	NULL, " +
 					 "QUESTION	TEXT	NOT	NULL, " +
 					 "ANSWER1	TEXT	NOT	NULL, " +
@@ -21,19 +21,19 @@ public class CreateDatabase {
 					 "ANSWER3	TEXT	NOT	NULL, " +
 					 "CORRECT	TEXT	NOT	NULL) ";
 		stmt.executeUpdate(sql);
-		sql = "INSERT INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
+		sql = "INSERT OR IGNORE INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
 				 "VALUES (1, 'This is a sample question 1', 'Sample answer 1 1', 'Sample answer 2 1', 'Sample answer 3 1', 'Correct answer 1' );";
 				stmt.executeUpdate(sql);
-		sql = "INSERT INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
+		sql = "INSERT OR IGNORE INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
 				 "VALUES (2, 'This is a sample question 2', 'Sample answer 1 2', 'Sample answer 2 2', 'Sample answer 3 2', 'Correct answer 2' );";
 				stmt.executeUpdate(sql);
-		sql = "INSERT INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
+		sql = "INSERT OR IGNORE INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
 				 "VALUES (3, 'This is a sample question 3', 'Sample answer 1 3', 'Sample answer 2 3', 'Sample answer 3 3', 'Correct answer 3' );";
 				stmt.executeUpdate(sql);
-		sql = "INSERT INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
+		sql = "INSERT OR IGNORE INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
 				 "VALUES (4, 'This is a sample question 4', 'Sample answer 1 4', 'Sample answer 2 4', 'Sample answer 3 4', 'Correct answer 4' );";
 				stmt.executeUpdate(sql);
-		sql = "INSERT INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
+		sql = "INSERT OR IGNORE INTO MULTIPLE (ID,QUESTION,ANSWER1,ANSWER2,ANSWER3,CORRECT) " +
 				 "VALUES (5, 'This is a sample question 5', 'Sample answer 1 5', 'Sample answer 2 5', 'Sample answer 3 5', 'Correct answer 5' );";
 		stmt.executeUpdate(sql);
 		stmt.close();
