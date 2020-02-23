3318d1cc3e8e22546d405e3e467816e0fd6ddda8
jacoberdman2147 <unconfigured@null.spigotmc.org>
2020-02-22 19:19:37 -0800

    Modified TmEventService to use a queue when handling events to prevent nested events from handling the lowest event before handling other cases of the outside event

--- src/triviaMaze/eventService/TmEventService.java
+++ src/triviaMaze/eventService/TmEventService.java
@@ -6,8 +6,13 @@ public class TmEventService {
 	// TODO: FIX THE FACT THAT EVENTS CAN BE CALLED AGAIN IN IMPROPER ORDER DUE TO EVENTS WITHIN EVENTS THAT ARE CAUGHT BY MORE THAN ONE HANDLER
 	
 	private static List<TmHandler> handlers;
+	private static Queue<String> eventQueue;
+	private static boolean currentlyHandling;
+	
 	static {
 		handlers = new LinkedList<TmHandler>();
+		eventQueue = new LinkedList<String>();
+		currentlyHandling = false;
 	}
 	
 	public static void addHandler(TmHandler handler) {
@@ -23,10 +28,20 @@ public class TmEventService {
 	}
 	
 	public static void fireEvent(String message) {
-		for (TmHandler handler : handlers) {
-			if (message.equals(handler.trigger)){ //This will likely need to be changed when swing is implemented and we can use proper key handling
-				handler.fire();
+		eventQueue.add(message);
+		if (!currentlyHandling) handleEventQueue();
+	}
+	
+	private static void handleEventQueue() {
+		currentlyHandling = true;
+		while (!eventQueue.isEmpty()) {
+			String message = eventQueue.remove();
+			for (int i = 0; i < handlers.size(); i++) {
+				if (message.equals(handlers.get(i).trigger)){ //This will likely need to be changed when swing is implemented and we can use proper key handling
+					handlers.get(i).fire();
+				}
 			}
 		}
+		currentlyHandling = false;
 	}
 }
