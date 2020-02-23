3ce6fe576a15b49bdbefc219dae70d1166c6898c
jacoberdman2147 <unconfigured@null.spigotmc.org>
2020-02-22 19:24:21 -0800

    Modified the question interface to make it more robust

--- src/triviaMaze/question/IQuestion.java
+++ src/triviaMaze/question/IQuestion.java
@@ -1,7 +1,7 @@
 package triviaMaze.question;
 
 public interface IQuestion {
-public void displayQuestion(String id);
-public void displayAnswers(String id);
-public void isCorrect(String id, String guess);
+	public String getQuestion();
+	public String[] getAnswers();
+	public boolean isCorrect(String guess);
 }
