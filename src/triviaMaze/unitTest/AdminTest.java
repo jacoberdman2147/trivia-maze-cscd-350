96e3b76178acfa738953743c552fd74ed19522b1
RandyHec <Randy.80@hotmail.com>
2020-03-07 14:32:44 -0800

    Very beginning of AdminTest class

---
+++ src/triviaMaze/unitTest/AdminTest.java
@@ -0,0 +1,18 @@
+package triviaMaze.unitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AdminTest {
+
+	public void testRemove() throws IOException{
+		String input = ""
+	}
+}
